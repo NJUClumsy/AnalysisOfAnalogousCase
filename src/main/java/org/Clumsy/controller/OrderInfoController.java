419e7efc1586b19fa2ad4879a433079499ed8352
Slow_Time <1395881075@qq.com>
2017-07-15 15:31:27 +0800

    增加测试用的配置文件

--- src/main/java/org/Clumsy/controller/OrderInfoController.java
+++ src/main/java/org/Clumsy/controller/OrderInfoController.java
@@ -5,6 +5,7 @@ import org.Clumsy.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -15,13 +16,14 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/orderinfo")
-public class UserInfoHandler {
+public class OrderInfoController {
 
     @Autowired
     private OrderService orderService;
 
+
     @ResponseBody
-    @RequestMapping("/showOrderByJson")
+    @RequestMapping(value = "/showOrderByJson", method = RequestMethod.GET)
     public List<Order> returnJson() {
         return orderService.getAllOrders();
     }
