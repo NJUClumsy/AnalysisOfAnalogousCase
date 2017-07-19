c3f79d4ba4f7b5ee209808902b7f69257411bcf5
Slow_Time <1395881075@qq.com>
2017-07-19 15:17:33 +0800

    java成功调用python

---
+++ src/test/java/org/Clumsy/service/SimilarCasesServiceTest.java
@@ -0,0 +1,23 @@
+package org.Clumsy.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by slow_time on 2017/7/19.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/test/resources/test-config.xml")
+public class SimilarCasesServiceTest {
+    @Autowired
+    SimilarCaseService similarCaseService;
+
+    @Test
+    public void testRecommendCases() throws Exception{
+        System.out.println(similarCaseService.recommendCases("596b2dbc39e14e6ddb1bb0b2"));
+    }
+}
