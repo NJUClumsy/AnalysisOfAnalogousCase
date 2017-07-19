82d0297214ef71d540c2b5dca21b0ba52e931686
NathanielLu <854806557@qq.com>
2017-07-19 10:33:12 +0800

    similarCaseImpl接口实现

--- src/main/java/org/Clumsy/service/impl/SimilarCaseServiceImpl.java
+++ src/main/java/org/Clumsy/service/impl/SimilarCaseServiceImpl.java
@@ -1,9 +1,19 @@
 package org.Clumsy.service.impl;
 
+import org.Clumsy.dao.CaseRepository;
+import org.Clumsy.entity.Case;
+import org.Clumsy.service.CaseService;
 import org.Clumsy.service.SimilarCaseService;
 import org.Clumsy.vo.CaseNumberVO;
+import org.Clumsy.vo.CaseVO;
+import org.python.core.PyFunction;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.util.PythonInterpreter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,9 +24,38 @@ import java.util.List;
 @Service("similarCaseService")
 public class SimilarCaseServiceImpl implements SimilarCaseService {
 
+    @Autowired
+    CaseRepository caseRepository;
 
     @Override
     public List<CaseNumberVO> recommendCases(String id) {
-        return null;
+        List<CaseNumberVO> list = new ArrayList<CaseNumberVO>();
+
+
+        CaseService caseService = new CaseServiceImpl();
+        CaseVO caseVO = caseService.getCaseInfoById(id);
+        String cause = caseVO.cause;
+
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.execfile("src/main/python/getSimilarID.py");//需要修改
+
+        PyFunction pyFunction = interpreter.get("getID", PyFunction.class); // 需要修改第一个参数为函数名字
+        PyObject pyObject = pyFunction.__call__(new PyString(id),new PyString(cause));
+
+        System.out.println(pyObject);
+
+        String instant = pyObject.toString();
+        instant = instant.substring(1,instant.length()-1);
+        System.out.println(instant);
+        String[] strList = instant.split(", ");
+        for (String o:strList){
+            o = o.substring(1,o.length()-1);
+            System.out.println(o);
+            Case instantCase = caseRepository.findById(o);
+            CaseNumberVO ins = new CaseNumberVO(instantCase.getId(),instantCase.getCaseNumber());
+            list.add(ins);
+        }
+
+        return list;
     }
 }
--- src/main/java/org/Clumsy/util/UserPthonHelper.java
+++ src/main/java/org/Clumsy/util/UserPthonHelper.java
@@ -19,6 +19,15 @@ public class UserPthonHelper {
         PyFunction pyFunction = interpreter.get("getID", PyFunction.class); // 第一个参数为期望获得的函数（变量）的名字，第二个参数为期望返回的对象类型
         PyObject pyObject = pyFunction.__call__(new PyInteger(1)); // 调用函数
 
+        String instant = pyObject.toString();
+        instant = instant.substring(1,instant.length()-1);
+        System.out.println(instant);
+        String[] oo = instant.split(", ");
+        for (String o:oo){
+            o = o.substring(1,o.length()-1);
+            System.out.println(o);
+        }
+
         System.out.println(pyObject);
     }
 }
